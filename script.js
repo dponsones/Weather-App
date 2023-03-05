6a16f4b455f83d15ebd9b6f88faa49402771a60c
Deion Ponsones <dcponson@gmail.com>
2023-03-05 13:31:06 -0500

    update JS

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
 var form =document.querySelector('form');
 let cityList = [];
+var cityHTMLText =document.createElement("p");
 
+// saves to local storage
 function saveCity () {
-  console.log("Saved cities")
-}
+  console.log("Saved cities");
+  localStorage.setItem('city', JSON.stringify(cityList));
+  loadCities()
+};
+
+// load on to page from local storage
+function loadCities () {
+  console.log("loaded cities", cityList);
+  let updatedCityList = JSON.parse(localStorage.getItem('city'));
+  cityHTMLText.textContent = updatedCityList;
+};
 
 // updates the search history
 function updateCityList() {
   console.log("updated jokes list");
-  for (var i = 0; i< cityList.length; i++) {
     var cityHTML = document.getElementById('city-list');
     var cityHTMLText =document.createElement("p");
-    var cityText = cityList[i];
+    var cityText = newCity;
     
     cityHTML.append(cityHTMLText);
     cityHTMLText.textContent =cityText;
     document.querySelector('form').reset();
-  };
- 
+    saveCity();
 };
 
 // adds city input to array list
 function addNewCity() {
   cityList.push(newCity);
   console.log("city list", cityList);
-  updateCityList()
+  updateCityList();
   
 };
 
@@ -33,11 +42,9 @@ function initListeners (event) {
     event.preventDefault();
     console.log("form-submit");
    
-   newCity= document.getElementById('new-city').value;
+   newCity = document.getElementById('new-city').value;
    console.log(newCity);
   
    addNewCity();
 };
-
- form.addEventListener('submit', initListeners);
-
+form.addEventListener('submit', initListeners);
